3c77ecfaba0b9eb67de3a51d8c82b150636d8af4
nickSwany <Thief.leroy@yandex.ru>
2024-04-05 02:37:57 +0300

    KS-1-4

---
+++ app/src/main/java/com/example/kotlinsprint/Task4.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinsprint
+
+
+fun main() {
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Double = 0.075
+    val seconds: Short = 6480
+    val partOfYear: Float = 2.0547945E-4f
+    val apogee: Int = 327000
+}

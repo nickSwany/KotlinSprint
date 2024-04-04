df361bd842e8d4b4aadef661c0f94fcf66409752
nickSwany <Thief.leroy@yandex.ru>
2024-04-05 02:40:48 +0300

    KS-1-5

---
+++ app/src/main/java/com/example/kotlinsprint/Task5.kt
@@ -0,0 +1,9 @@
+package com.example.kotlinsprint
+
+fun main() {
+    val minutesInSpace: Int = 106
+    val minutes = 106 - 60
+    val hours = "01"
+    val secoonds = "00"
+    println("$hours:$minutes:$secoonds")
+}

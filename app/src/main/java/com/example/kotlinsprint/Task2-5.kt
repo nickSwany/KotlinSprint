d56b588ee3408be3391498386a1f1493fee64b93
nickSwany <Thief.leroy@yandex.ru>
2024-04-07 00:35:17 +0300

    KS-2-5 правки (добавил пробелы, про функцию авторастановки пробелов знаю, однако еще не всегда пользуюсь ей)

--- app/src/main/java/com/example/kotlinsprint/Task2-5.kt
+++ app/src/main/java/com/example/kotlinsprint/Task2-5.kt
@@ -2,13 +2,13 @@ package com.example.kotlinsprint
 
 import kotlin.math.pow
 
-fun main(){
+fun main() {
 
     val money = 70_000
     val percentStage = 16.7
     val years = 20
 
-    val results = ((1+percentStage/100).pow(years) * money)
+    val results = ((1 + percentStage / 100).pow(years) * money)
 
     println(String.format("%.3f", results))
 }

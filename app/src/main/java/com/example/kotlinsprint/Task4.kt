440715c16e9b1118dae4279dbf6e2ed36afb811c
nickSwany <Thief.leroy@yandex.ru>
2024-04-06 03:46:03 +0300

    KS-1-4 правки

--- app/src/main/java/com/example/kotlinsprint/Task4.kt
+++ app/src/main/java/com/example/kotlinsprint/Task4.kt
@@ -4,8 +4,8 @@ package com.example.kotlinsprint
 fun main() {
     val length: Long = 40_868_600_000
     val age: Byte = 27
-    val partOfDay: Double = 0.075
+    val partOfDay: Float = 0.075f
     val seconds: Short = 6480
-    val partOfYear: Float = 2.0547945E-4f
+    val partOfYear: Float = 2.0547945205479453E-4f //Тут AS предлогает сама сократить данное значение(подсвечивается желтым)
     val apogee: Int = 327000
 }

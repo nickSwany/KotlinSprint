7fe6a6c8d4e46adc1d2afe2815c527e197ebb01a
nickSwany <Thief.leroy@yandex.ru>
2024-04-07 00:58:38 +0300

    KS-3-1 Исправил орфографическую ошибку

--- app/src/main/java/com/example/kotlinsprint/Task-3-1.kt
+++ app/src/main/java/com/example/kotlinsprint/Task-3-1.kt
@@ -1,14 +1,14 @@
 package com.example.kotlinsprint
 
 fun main() {
-    val nameUssers = "Никита"
+    val nameUsers = "Никита"
     val enterTextDay = "Добрый день!"
     val enterTextNight = "Добрый вечер!"
 
     println(
         """
-        $enterTextDay $nameUssers.
-        $enterTextNight $nameUssers.
+        $enterTextDay $nameUsers.
+        $enterTextNight $nameUsers.
     """.trimIndent()
     )
 }

35dac18002ee115e2b237bd42dfa8b69cfe7a18b
nickSwany <Thief.leroy@yandex.ru>
2024-04-09 19:06:14 +0300

    KS-5-3

---
+++ app/src/main/java/com/example/kotlinsprint/Task-5-3.kt
@@ -0,0 +1,21 @@
+package com.example.kotlinsprint
+
+const val NUMBER_ONE = 11
+const val NUMBER_TWO = 27
+
+fun main() {
+    println("Нужно угадать два числа от 0 до 42\nВведите первое число")
+    val userNumberOne = readln()!!.toInt()
+    println("Введите второе число")
+    val userNumberTwo = readLine()!!.toInt()
+
+    val result =
+        if ((userNumberOne == NUMBER_ONE && userNumberTwo == NUMBER_TWO) || (userNumberOne == NUMBER_TWO && userNumberTwo == NUMBER_ONE))
+            "Поздравляем вы выйграли главный приз!"
+        else if ((userNumberOne == NUMBER_TWO || userNumberTwo == NUMBER_ONE) || (userNumberOne == NUMBER_ONE || userNumberTwo == NUMBER_TWO))
+            "Вы выйграли утешительный приз!"
+        else "Неудача!"
+
+    println(result)
+    println("Правильные числа для победы были: $NUMBER_ONE и $NUMBER_TWO")
+}

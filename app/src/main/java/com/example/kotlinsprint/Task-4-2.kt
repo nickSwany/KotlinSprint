aa547ba59ec6631984610cefc62460d89a5ac911
nickSwany <Thief.leroy@yandex.ru>
2024-04-10 17:33:20 +0300

    KS-4-2

--- app/src/main/java/com/example/kotlinsprint/Task-4-2.kt
+++ app/src/main/java/com/example/kotlinsprint/Task-4-2.kt
@@ -8,9 +8,21 @@ fun main() {
 
     var weight = 20
     var volume = 80
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${(weight > MIN_WEIGHT && weight <= MAX_WEIGHT) && (volume < VOLUME_IN_LITERS)}")
+    println(
+        "Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': " +
+                "${
+                    (weight > MIN_WEIGHT && weight <= MAX_WEIGHT) &&
+                            (volume < VOLUME_IN_LITERS)
+                }"
+    )
 
     weight = 50
     volume = 100
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${(weight > MIN_WEIGHT && weight <= MAX_WEIGHT) && (volume < VOLUME_IN_LITERS)}")
+    println(
+        "Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': " +
+                "${
+                    (weight > MIN_WEIGHT && weight <= MAX_WEIGHT) &&
+                            (volume < VOLUME_IN_LITERS)
+                }"
+    )
 }

082f8555b3e7ca4f075d5772e3900e6e6911b9ad
nickSwany <Thief.leroy@yandex.ru>
2024-04-11 18:32:41 +0300

    KS-5-2 Правки

--- app/src/main/java/com/example/kotlinsprint/Task-5-2.kt
+++ app/src/main/java/com/example/kotlinsprint/Task-5-2.kt
@@ -2,17 +2,27 @@ package com.example.kotlinsprint
 
 const val THIS_YEAR = 2024
 const val MIN_AGE = 18
+const val SIXTEEN_AGE = 16
+const val SEVENTEEN_AGE = 17
 
 fun main() {
     println("Введите год рождения")
 
-    val ageOfBirth = readLine()!!.toInt()
+    val ageOfBirth = readln().toIntOrNull()
 
-    val userAge = THIS_YEAR - ageOfBirth
+    if (ageOfBirth == null) println("Вы не ввели год рождения!")
+    else {
+        val userAge = THIS_YEAR - ageOfBirth
 
-    val resultText = if (userAge >= MIN_AGE) "Показать экран со скрытым контентом"
-    else if (userAge == 16 || userAge == 17) "Показать экран с ограниченным контентом"
-    else "Вернуться на главный экран"
-
-    println(resultText)
+        val resultText =
+            when {
+                userAge >= MIN_AGE -> "Показать экран со скрытым контентом"
+                (userAge == SIXTEEN_AGE) ||
+                        (userAge == SEVENTEEN_AGE) -> "Показать экран с ограниченным контентом"
+                else -> {
+                    "Вернуться на главный экран"
+                }
+            }
+        println(resultText)
+    }
 }

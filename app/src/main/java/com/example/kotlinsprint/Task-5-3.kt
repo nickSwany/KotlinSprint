c258839e76c85ebd7830bba064ccd629e6a82ef7
nickSwany <Thief.leroy@yandex.ru>
2024-04-11 18:20:36 +0300

    KS-5-3 Правки

--- app/src/main/java/com/example/kotlinsprint/Task-5-3.kt
+++ app/src/main/java/com/example/kotlinsprint/Task-5-3.kt
@@ -4,15 +4,21 @@ const val NUMBER_ONE = 11
 const val NUMBER_TWO = 27
 
 fun main() {
+
     println("Нужно угадать два числа от 0 до 42\nВведите первое число")
-    val userNumberOne = readln()!!.toInt()
+    val userNumberOne = readln().toIntOrNull()
+
     println("Введите второе число")
-    val userNumberTwo = readLine()!!.toInt()
+    val userNumberTwo = readln().toIntOrNull()
 
     val result =
-        if ((userNumberOne == NUMBER_ONE && userNumberTwo == NUMBER_TWO) || (userNumberOne == NUMBER_TWO && userNumberTwo == NUMBER_ONE))
+        if ((userNumberOne == NUMBER_ONE && userNumberTwo == NUMBER_TWO) ||
+            (userNumberOne == NUMBER_TWO && userNumberTwo == NUMBER_ONE)
+        )
             "Поздравляем вы выйграли главный приз!"
-        else if ((userNumberOne == NUMBER_TWO || userNumberTwo == NUMBER_ONE) || (userNumberOne == NUMBER_ONE || userNumberTwo == NUMBER_TWO))
+        else if ((userNumberOne == NUMBER_TWO || userNumberTwo == NUMBER_ONE) ||
+            (userNumberOne == NUMBER_ONE || userNumberTwo == NUMBER_TWO)
+        )
             "Вы выйграли утешительный приз!"
         else "Неудача!"
 

a72a64d066381db25a1bf7089cebb092cefd6b46
nickSwany <Thief.leroy@yandex.ru>
2024-04-11 18:51:12 +0300

    KS-4-5 Правки

--- app/src/main/java/com/example/kotlinsprint/Task-4-5.kt
+++ app/src/main/java/com/example/kotlinsprint/Task-4-5.kt
@@ -7,23 +7,38 @@ const val SUPPLIES_BOX = 50
 fun main() {
 
     println("Имеются ли на корабле повреждения?\nНеобходимо ответить: true/false")
-    val shipIntegrity = readLine()!!.toBoolean()
+    val shipIntegrity = readln().toBooleanStrictOrNull()
 
     println("Введите количество людей в экипаже")
-    val crew = readLine()!!.toInt()
+    val crew = readln().toIntOrNull()
 
     println("Введите количество ящиков с провизией")
-    val boxOfSupplies = readLine()!!.toInt()
+    val boxOfSupplies = readln().toIntOrNull()
 
     println("Благоприятная ли погда?\nНеобходимо ответить: true/false")
-    val weather = readLine()!!.toBoolean()
+    val weather = readln().toBooleanStrictOrNull()
 
-    if (!shipIntegrity &&
-        (crew in MIN_CREW..MAX_CREW) &&
-        (boxOfSupplies > SUPPLIES_BOX) &&
-        weather ||
-        (crew == MAX_CREW) &&
-        (boxOfSupplies > SUPPLIES_BOX))
-        println("Отбытие разрешено")
-    else println("Отбытие ЗАПРЕЩЕНО!!!")
+    if (boxOfSupplies != null && shipIntegrity != null &&
+        crew != null && weather != null
+    ) {
+        if ((!shipIntegrity &&
+                    (crew in MIN_CREW..MAX_CREW) &&
+                    (boxOfSupplies > SUPPLIES_BOX) &&
+                    weather) ||
+            ((crew == MAX_CREW) &&
+                    (boxOfSupplies > SUPPLIES_BOX))
+        )
+        /* Не совсем понял по комментарию как лучше будет. Так как я сделал выше или тот вариант
+        Который ниже?
+        (!shipIntegrity &&
+                ((crew in MIN_CREW..MAX_CREW) &&
+                (boxOfSupplies > SUPPLIES_BOX)) &&
+                weather ||
+        ((crew == MAX_CREW) &&
+                (boxOfSupplies > SUPPLIES_BOX))
+    )
+         */
+            println("Отбытие разрешено")
+        else println("Отбытие ЗАПРЕЩЕНО!!!")
+    } else println("Необходимо ввести все данные для запроса на отбытие!")
 }

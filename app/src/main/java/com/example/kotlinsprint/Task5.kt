8bc3a7c1b188b9ef69f611c9fc4b6361cce94c8c
nickSwany <Thief.leroy@yandex.ru>
2024-04-12 17:36:08 +0300

    KS-1-5 Правки

--- app/src/main/java/com/example/kotlinsprint/Task5.kt
+++ app/src/main/java/com/example/kotlinsprint/Task5.kt
@@ -1,9 +1,17 @@
 package com.example.kotlinsprint
 
+const val SECONDS_IN_MINUTES = 60
+const val MINUTES_IN_SPACE = 108
+const val SECONDS_IN_HOURS = 3600
 fun main() {
-    val minutesInSpace: Int = 106
-    val minutes = 106 - 60
-    val hours = "01"
-    val secoonds = "00"
-    println("$hours:$minutes:$secoonds")
+    val secondsInSpace: Int = MINUTES_IN_SPACE * SECONDS_IN_MINUTES
+
+    val hours = secondsInSpace / SECONDS_IN_HOURS
+    val remainingSexonds = secondsInSpace % SECONDS_IN_HOURS
+
+    val minutes = remainingSexonds / SECONDS_IN_MINUTES
+    val remainingMinutes = remainingSexonds % SECONDS_IN_MINUTES
+
+
+    println("Гагарин был в космосе в течение: ${String.format("%02d:%02d:%02d", hours, minutes, remainingMinutes)}")
 }

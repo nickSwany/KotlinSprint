c4f50e87169cb2373e7370590a801a15e2716e4f
nickSwany <Thief.leroy@yandex.ru>
2024-04-15 18:57:52 +0300

    KS-3-5 Правки №2

--- app/src/main/java/com/example/kotlinsprint/Task-3-5.kt
+++ app/src/main/java/com/example/kotlinsprint/Task-3-5.kt
@@ -3,21 +3,9 @@ package com.example.kotlinsprint
 fun main() {
     val motion = "D2-D4;0"
 
-    val listMotion: List<String> = motion.split('-',';')
+    val listMotion: List<String> = motion.split('-', ';')
 
     println(listMotion[0])
     println(listMotion[1])
     println(listMotion[2])
-
-//    val startMotion = motion.substringBefore('-')
-//    val endMotion = motion.substringAfter('-').substringBefore(';')
-//    val curMotion = motion.substringAfter(';')
-//
-//    println(
-//        """
-//        $startMotion
-//        $endMotion
-//        $curMotion
-//    """.trimIndent()
-//    )
 }

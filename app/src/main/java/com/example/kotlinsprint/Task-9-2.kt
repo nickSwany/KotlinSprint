058a0c09a8fc6c0fa256bab3d82c1bf76ed80a84
nickSwany <Thief.leroy@yandex.ru>
2024-04-18 13:54:17 +0300

    KS-9-2

---
+++ app/src/main/java/com/example/kotlinsprint/Task-9-2.kt
@@ -0,0 +1,23 @@
+package com.example.kotlinsprint
+
+fun main() {
+
+    val basicIngredients = mutableListOf("сыр", "мясо", "помидор")
+
+    println("В рецепте есть базовые ингредиенты:")
+    basicIngredients.forEach { println(it) }
+
+    println("Желаете добавить еще? Да или Нет.")
+
+    val userAnswer = readln()
+
+    if (userAnswer.lowercase() == "да") {
+        println("Какой ингредиент желаете добавить?")
+        val userIngredients = readln()
+
+        basicIngredients.add(userIngredients)
+
+        println("Теперь в рецепту есть следующие ингредиенты:")
+        basicIngredients.forEach { println(it) }
+    } else return
+}
